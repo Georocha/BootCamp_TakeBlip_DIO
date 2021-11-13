e16cb9d3ea58a14f98ed52624971d77649638654
Geovane Rocha <71407456+Georocha@users.noreply.github.com>
2021-11-13 20:00:40 -0300

    Add files via upload

---
+++ CSharp/Classe e Objetos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rocha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
